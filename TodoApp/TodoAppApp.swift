efc57df4d37fa09ac84cb0c47f2af3449e641d93
MorumBee <zun0323@gmail.com>
2025-03-13 15:54:27 -0300

    Initial Commit

---
+++ TodoApp/TodoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoAppApp.swift
+//  TodoApp
+//
+//  Created by Morumbi on 3/13/25.
+//
+
+import SwiftUI
+
+@main
+struct TodoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
